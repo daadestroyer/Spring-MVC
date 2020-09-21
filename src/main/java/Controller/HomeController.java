eaee28d38c101feb0a4f7bc6bbd16e6c6404b122
kool nigam <nigamshubham2000@gmail.com>
2020-09-21 10:39:44 +0530

    Today i learned how to send data to view part using model.AddAttribute() funtion

--- src/main/java/Controller/HomeController.java
+++ src/main/java/Controller/HomeController.java
@@ -1,24 +1,36 @@
 package Controller;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("name", "Shubham Nigam");
+		model.addAttribute("age", 21);
+		List<String> friend = new ArrayList<String>();
+		friend.add("shubhanshu");
+		friend.add("sakshi");	
+		model.addAttribute("friends",friend);
 		return "index";
 	}
+
 	@RequestMapping("/about")
 	public String about() {
 		return "about";
 	}
+
 	@RequestMapping("/services")
 	public String services() {
 		return "services";
 	}
+
 	@RequestMapping("/contact")
 	public String contact() {
 		return "contact";
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,6 @@
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +8,29 @@
 <title>Home Page</title>
 </head>
 <body>
-<center>
-<h1>Home page</h1>
-</center>
+	<center>
+		<%
+			String name = (String) request.getAttribute("name");
+		Integer age = (Integer) request.getAttribute("age");
+		List<String> friends = (List<String>) request.getAttribute("friends");
+		%>
+		<h1>Home page</h1>
+		<h2>
+			<span style="color: red;">Welcome</span> ,
+			<%=name%>
+			your age is
+			<%=age%></h2>
+		<h2>
+			Your friends are :
+			<%
+			for (String s : friends) {
+		%>
+			<%
+				out.println(s);
+			}
+			%>
+
+		</h2>
+	</center>
 </body>
 </html>
